a6d21ad27bd25e7c059d4bb3df0bf5b415cd12e0
cendekiapp <cendiqkrn@gmail.com>
2015-05-12 16:25:53 +0700

    16_Ogres Are Like Middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,12 +10,18 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
-
-Route::resource('articles', 'ArticlesController');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController'
 ]);
+
+Route::group(['prefix' => Config::get('admin.urlPrefix'), 'middleware' => 'auth'], function()
+{
+	Route::resource('articles', 'ArticlesController');
+});
+
+
